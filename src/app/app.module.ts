80a557785af92a83a1efbcd2b71f88eb078bae00
Prats <pratsa@ap-localadmins-MacBook-Pro-2.local>
2017-06-02 16:42:45 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { RemoteComponent } from './remote_component/app.remote-component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RemoteComponent
   ],
   imports: [
     BrowserModule,
